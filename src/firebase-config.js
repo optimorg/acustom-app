422a9d37f841b03a10d325fb34d5828fac6ef2cc
optimorg <73851301+optimorg@users.noreply.github.com>
2023-09-24 20:45:58 -0400

    Added and initialized firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDf19y-gUmpSCydMan7t3feCsujtgACMG4",
+  authDomain: "acustom-ecomm.firebaseapp.com",
+  databaseURL: "https://acustom-ecomm-default-rtdb.firebaseio.com",
+  projectId: "acustom-ecomm",
+  storageBucket: "acustom-ecomm.appspot.com",
+  messagingSenderId: "282364937406",
+  appId: "1:282364937406:web:89789cde75bd94c714caef"
+};
