281623b37bfffdfd054839dc5c767684a205edb7
morgREEN <morgreen25@gmail.com>
2023-10-02 14:19:26 -0400

    Fixed- app initialized with firebase again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideFunctions,getFunctions } from '@angular/fire/functions';
-import { provideMessaging,getMessaging } from '@angular/fire/messaging';
-import { providePerformance,getPerformance } from '@angular/fire/performance';
-import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
-import { provideStorage,getStorage } from '@angular/fire/storage';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AuthComponent } from './shared/auth/auth.component';
-import { ComponentComponent } from './component/component.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+
+import {environment} from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    DashboardComponent,
-    AuthComponent,
-    ComponentComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAnalytics(() => getAnalytics()),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore()),
-    provideFunctions(() => getFunctions()),
-    provideMessaging(() => getMessaging()),
-    providePerformance(() => getPerformance()),
-    provideRemoteConfig(() => getRemoteConfig()),
-    provideStorage(() => getStorage()),
+    AngularFireModule.initializeApp(environment.firebase),
   ],
-  providers: [
-
-  
-    ScreenTrackingService,UserTrackingService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+  firebase: {
+    projectId: 'acustom-ecomm',
+    appId: '1:282364937406:web:89789cde75bd94c714caef',
+    databaseURL: 'https://acustom-ecomm-default-rtdb.firebaseio.com',
+    storageBucket: 'acustom-ecomm.appspot.com',
+    apiKey: 'AIzaSyDf19y-gUmpSCydMan7t3feCsujtgACMG4',
+    authDomain: 'acustom-ecomm.firebaseapp.com',
+    messagingSenderId: '282364937406',
+  }
+};
