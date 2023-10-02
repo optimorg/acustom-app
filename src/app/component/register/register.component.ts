062bb92774fde7aef3610939867bbde97992a222
morgREEN <morgreen25@gmail.com>
2023-10-02 16:53:48 -0400

    Added register auth method and binded to html file reg fields

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
- 
+export class RegisterComponent implements OnInit {
+
+  email : string = '';
+  password : string = '';
+
+  constructor(private auth: AuthService) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  register() {
+    if(this.email == '') {
+      alert('Email field is empty. Please enter email address.')
+    }
+    if(this.password == '') {
+      alert('Password field is empty. Please enter password.')
+    }
+
+    this.auth.register(this.email, this.password);
+    this.email = ''
+    this.password = ''
+  }
   }
