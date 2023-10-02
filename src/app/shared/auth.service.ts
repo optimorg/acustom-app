450da1efc274d377249d3b18f3a5c86e575c1449
morgREEN <morgreen25@gmail.com>
2023-10-02 14:32:59 -0400

    Added login auth

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
-}
+  constructor(private fireAuth : AngularFireAuth, private router : Router) { }
+
+  //login method
+  login(email : string, password : string) {
+    this.fireAuth.signInWithEmailAndPassword(email, password).then( () => {
+        localStorage.setItem('token','true');
+        this.router.navigate(['dashboard']);
+    }, err => {
+        alert('Something went wrong');
+        this.router.navigate(['/login']);
+    })//end of then
+  }//end of login method
+}//end of Auth Service
