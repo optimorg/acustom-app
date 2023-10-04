df7c1b44ebe93e1dbacb9beca1ccf8ae073099a8
morgREEN <morgreen25@gmail.com>
2023-10-03 20:15:46 -0400

    Added dashboard nav and logout button

--- src/app/component/dashboard/navigation/navigation.component.ts
+++ src/app/component/dashboard/navigation/navigation.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/fireauth/auth.service'; 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
 
+  constructor(private auth: AuthService, private router: Router) {}
+  ngOnInit(): void {
+    
+  }
+
+  logout() {
+    this.auth.logout();
+    localStorage.removeItem('token');
+    alert('Logged out successfully');
+    this.router.navigate(['/login']);
+  }
 }
