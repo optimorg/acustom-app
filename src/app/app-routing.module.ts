5ee4924af03a708bce48464cf778922971463f51
morgREEN <morgreen25@gmail.com>
2023-10-04 15:51:47 -0400

    Created order form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { OrderFormComponent } from './component/orders/order-form/order-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path:"register", component: RegisterComponent },
-  { path:'dashboard', component: DashboardComponent }
+  { path:'dashboard', component: DashboardComponent },
+  { path:'order-form', component: OrderFormComponent },
+
 ];
 
 @NgModule({
